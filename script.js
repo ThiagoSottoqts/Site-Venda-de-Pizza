d47f5b25581a287228547ff6c73595da516c13be
Thiago Sotto <thiago.sotto@hotmail.com>
2021-11-18 19:55:55 -0300

    Finalizado

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let cart = []
+let modalQt = 1
+let modalKey = 0 
 
 const c = (el)=> {   
     return document.querySelector(el)
@@ -5,16 +8,41 @@ const c = (el)=> {
 
 const cs = (el)=>document.querySelectorAll(el) // outra forma de fazer o atalho sem o return
 
+                                    //Listagem das pizzas
 pizzaJson.map((item, index)=>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true)
 
+    pizzaItem.setAttribute('data-key', index) //onde vai gravar a informação da pizza que esta sendo comprada
     pizzaItem.querySelector('.pizza-item--img img').src = item.img
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+
+                                    //Eventos de click para abrir o modal
     pizzaItem.querySelector('a').addEventListener('click', (e)=>{
         e.preventDefault()
 
+        let key = e.target.closest('.pizza-item').getAttribute('data-key')// para pegar a pizza clicada
+        modalQt = 1 //para sempre que abrir uma piza estar em 1
+        modalKey = key //com essa variavel eu sei qual pizza foi selicionada
+       
+        c('.pizzaBig img').src = pizzaJson[key].img //prencher com a img da pizza
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name //prencher o nome no modal com a pizza clicada
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description//prencher com adescrição da pizza
+        c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`//prencher com 0 preço
+        c('.pizzaInfo--size.selected').classList.remove('selected')//remover o selected da pizza grande
+
+        cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+            if(sizeIndex == 2) {
+                size.classList.add('selected') // para deixar a pizza grande sempre selecionada
+            }
+
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex] //tamanho das pizzas
+
+        })
+
+        c('.pizzaInfo--qt').innerHTML = modalQt
+        
         c('.pizzaWindowArea').style.opacity = 0
         c('.pizzaWindowArea').style.display = 'flex' // para aparecer o modal
         setTimeout(()=>{
@@ -26,3 +54,132 @@ pizzaJson.map((item, index)=>{
 
     c('.pizza-area').append( pizzaItem )
 })
+
+                                            //Eventos do modal
+
+function closeModal() {
+    c('.pizzaWindowArea').style.opacity = 0
+    setTimeout(()=>{
+        c('.pizzaWindowArea').style.display = 'none'
+    },500)
+}
+
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{
+    item.addEventListener('click', closeModal)
+})
+
+c('.pizzaInfo--qtmenos').addEventListener('click', ()=>{
+    if(modalQt > 1) {
+    modalQt--
+    c('.pizzaInfo--qt').innerHTML = modalQt
+    }
+})
+
+c('.pizzaInfo--qtmais').addEventListener('click', ()=>{
+    modalQt++
+    c('.pizzaInfo--qt').innerHTML = modalQt
+})
+
+cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+    size.addEventListener('click', (e)=>{
+        c('.pizzaInfo--size.selected').classList.remove('selected')
+        size.classList.add('selected')
+    })
+})
+
+c('.pizzaInfo--addButton').addEventListener('click', ()=>{
+    let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'))
+    
+    let identifier = pizzaJson[modalKey].id+'@'+size;
+
+    let key = cart.findIndex((item)=>item.identifier == identifier);
+
+    if(key > -1) {
+        cart[key].qt += modalQt;
+    } else {
+        cart.push({
+            identifier,
+            id:pizzaJson[modalKey].id,
+            size,
+            qt:modalQt
+    });
+}
+    updateCart();
+    closeModal()
+});
+
+c('.menu-openner').addEventListener('click', ()=> {
+    if(cart.length > 0) {
+        c('aside').style.left = '0'
+    }
+})
+
+c('.menu-closer').addEventListener('click', ()=> {
+    c('aside').style.left = '100vw'
+})
+
+function updateCart() {
+    c('.menu-openner span').innerHTML = cart.length
+
+    if(cart.length > 0) {
+        c('aside').classList.add('show');
+        c('.cart').innerHTML = '';
+
+        let subtotal = 0
+        let desconto = 0
+        let total = 0
+
+        for(let i in cart) {
+            let pizzaItem = pizzaJson.find((item)=>item.id == cart[i].id);
+            subtotal += pizzaItem.price * cart[i].qt
+
+            let cartItem = c('.models .cart--item').cloneNode(true);
+
+            let pizzaSizeName
+            switch (cart[i].size) {
+                case 0:
+                    pizzaSizeName = 'P'
+                    break;
+
+                    case 1:
+                    pizzaSizeName = 'M'
+                    break;
+
+                    case 2:
+                    pizzaSizeName = 'G'
+                    break;
+            }
+            let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
+
+            cartItem.querySelector('img').src = pizzaItem.img;
+            cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
+            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click',()=>{
+                if(cart[i].qt > 1) {
+                    cart[i].qt--;
+                } else {
+                    cart.splice(i, 1)
+                }
+                    updateCart()
+                
+            });
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click',()=>{
+                cart[i].qt++
+                updateCart()
+            })
+
+            c('.cart').append(cartItem)
+        }
+
+        desconto = subtotal * 0.1
+        total = subtotal - desconto
+
+        c('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2)}`
+        c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`
+        c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`
+
+    } else {
+        c('aside').classList.remove('show');
+        c('aside').style.left = '100vw'
+    }
+}
